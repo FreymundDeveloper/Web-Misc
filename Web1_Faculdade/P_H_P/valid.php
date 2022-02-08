c96c68e9a519d5464aeb8493f56459e58f12075f
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-02-07 22:42:35 -0300

    ??

--- Web1_Faculdade/P_H_P/valid.php
+++ Web1_Faculdade/P_H_P/valid.php
@@ -18,9 +18,28 @@
             die ( );
         }
 
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false){
+            print('Email invalido');
+            die();
+        }
+
         if ( empty($_POST['bday']) {
             print ("Data de nascimento obrigatorio");
             die ( );
+        }
+
+        $data = explode('-', $_POST['bday']));
+        $dataLimite = new DateTime('2021-02-07');
+        $dataUsr = new DateTime($_POST['bday']);
+
+        if(count($data) != 3 || checkdate($data[1], $data[2], $data[0]) == false){
+            print('Data de nascimento invalida');
+            die();
+        }
+
+        if($dataUsr > $dataLimite){//Não entendi muito bem o que tinha q fazer, enunciado meio confuso
+            print('Idade digitada ultrapassa o dia atual');
+            die();
         }
 
         if ( empty($_POST['usrtel']) {
@@ -28,14 +47,19 @@
             die ( );
         }
 
+        if(preg_match("/\([0-9]{2}\)[0-9]{4}\-[0-9]{4}$/", $_POST['usrtel']) != 1) {
+            print('Telefone invalido');
+            die();
+        }
 
-	    print("Seus dados:");
-	    print("Nome: $_POST['nome']");
-	    print("Email: $_POST['email']");
-        print("Data de Nascimento: $_POST['bday']");
-	    print("Telefone: $_POST['usrtel']");  
-        print("URL: $_POST['homepage']");    
-        print("Estado: $_POST['estados']"); 
+
+	    print("<br>Seus dados:<br>");
+	    print("Nome: $_POST['nome']<br>");
+	    print("Email: $_POST['email']<br>");
+        print("Data de Nascimento: $_POST['bday']<br>");
+	    print("Telefone: $_POST['usrtel']<br>");  
+        print("URL: $_POST['homepage']<br>");    
+        print("Estado: $_POST['estados']<br>"); 
     ?>
 
     
