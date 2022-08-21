48a2afbc737e2cb182a947daa99f0f6753aabdd7
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-08-20 23:49:18 -0300

    Project Next, D2

--- Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
+++ Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className='flex'>
-      <span></span>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-violet-500 to-blue-500
+      text-white
+    `}>
+      <Layout titulo='Cadastro'>
+        <span>Content</span>
+      </Layout>
     </div>
   )
 }
