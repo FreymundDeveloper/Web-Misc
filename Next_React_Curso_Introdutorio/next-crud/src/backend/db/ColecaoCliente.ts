b5174cb1efadf235cc171611c697a3c0137f592f
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-09-01 19:10:15 -0300

    Firebase worked

--- Next_React_Curso_Introdutorio/next-crud/src/backend/db/ColecaoCliente.ts
+++ Next_React_Curso_Introdutorio/next-crud/src/backend/db/ColecaoCliente.ts
@@ -3,7 +3,7 @@ import Cliente from "../../core/Cliente";
 import ClienteRepositorio from "../../core/ClienteRepositorio";
 
 export default class ColecaoCliente implements ClienteRepositorio {
-    coversor = {
+    #conversor = {
         toFirestore(cliente: Cliente) {
             return {
                 nome: cliente.nome,
@@ -11,18 +11,36 @@ export default class ColecaoCliente implements ClienteRepositorio {
             }
         },
 
-        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions) {}
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions) {
+            const dados = snapshot.data(options)
+            return new Cliente(dados.nome, dados.idade, snapshot.id)
+        }
     }
 
     async salvar(cliente: Cliente): Promise<Cliente> {
-        return null
+        if(cliente?.id) {
+            await this.#colecao().doc(cliente.id).set(cliente)
+            return cliente
+        } else {
+            const docRef = await this.#colecao().add(cliente)
+            const doc = await docRef.get()
+            return doc.data()
+        }
     }
 
     async excluir(cliente: Cliente): Promise<void> {
-        return null
+        return this.#colecao().doc(cliente.id).delete()
     }
 
     async obterTodos(): Promise<Cliente[]> {
-        return null
+        const query = await this.#colecao().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
+
+    #colecao() {
+        return firebase
+        .firestore()
+        .collection('clientes')
+        .withConverter(this.#conversor)
     }
 }
