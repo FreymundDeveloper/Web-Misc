c32da9d1572c926bb287d79e8bbfa58c2a79cea2
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-09-01 05:03:28 -0300

    Firebase sucks

---
+++ Next_React_Curso_Introdutorio/next-crud/src/backend/db/ColecaoCliente.ts
@@ -0,0 +1,28 @@
+import firebase from "../config";
+import Cliente from "../../core/Cliente";
+import ClienteRepositorio from "../../core/ClienteRepositorio";
+
+export default class ColecaoCliente implements ClienteRepositorio {
+    coversor = {
+        toFirestore(cliente: Cliente) {
+            return {
+                nome: cliente.nome,
+                idade: cliente.idade,
+            }
+        },
+
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions) {}
+    }
+
+    async salvar(cliente: Cliente): Promise<Cliente> {
+        return null
+    }
+
+    async excluir(cliente: Cliente): Promise<void> {
+        return null
+    }
+
+    async obterTodos(): Promise<Cliente[]> {
+        return null
+    }
+}
