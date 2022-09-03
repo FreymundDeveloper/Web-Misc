0660e2280c990b5f0659c263f2a004a15ed097a6
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-09-03 04:10:36 -0300

    PN, D7

--- Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
+++ Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
@@ -1,37 +1,48 @@
 import { ClientRequest } from "http";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
 
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
 
-  const clientes = [
-    new Cliente('Jack', 34, '1'),
-    new Cliente('John', 73, '2'),
-    new Cliente('Vi', 32, '3'),
-    new Cliente('Tedd', 44, '4'),
-  ]
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clientSelect (cliente: Cliente) {
     setCliente(cliente)
     setVisivel('form')
   }
 
-  function clientDelet (cliente: Cliente) {}
+  async function clientDelet (cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
+  }
 
   function clientNew () {
     setCliente(Cliente.vazio())
     setVisivel('form')
   }
 
-  function clientSave (cliente: Cliente) {
-    setVisivel('tabela')
+  async function clientSave (cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos
   }
 
   return (
