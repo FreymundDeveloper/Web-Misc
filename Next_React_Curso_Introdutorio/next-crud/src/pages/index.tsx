54ea72a86d429a0aa618a31871285e82d67ac037
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-09-10 02:30:02 -0300

    PN, finished

--- Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
+++ Next_React_Curso_Introdutorio/next-crud/src/pages/index.tsx
@@ -7,43 +7,11 @@ import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import ClienteRepositorio from "../core/ClienteRepositorio";
+import useClientes from "../hooks/useClientes";
 
 export default function Home() {
 
-  const repo: ClienteRepositorio = new ColecaoCliente()
-
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes, setClientes] = useState<Cliente[]>([])
-
-  useEffect(obterTodos, [])
-
-  function obterTodos() {
-    repo.obterTodos().then(clientes => {
-      setClientes(clientes)
-      setVisivel('tabela')
-    })
-  }
-
-  function clientSelect (cliente: Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  async function clientDelet (cliente: Cliente) {
-    await repo.excluir(cliente)
-    obterTodos()
-  }
-
-  function clientNew () {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  async function clientSave (cliente: Cliente) {
-    await repo.salvar(cliente)
-    obterTodos
-  }
+  const { clientSelect, clientDelet, clientSave, clientNew, cliente, clientes, tabelaVisivel, exibirTabela } = useClientes()
 
   return (
     <div className={`
@@ -52,7 +20,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo='Cadastro'>
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className="flex justify-end">
               <Botao cor="blue" className="mb-4" 
@@ -70,7 +38,7 @@ export default function Home() {
           <Formulario 
           cliente={cliente}
           clienteMudou={clientSave}
-          cancelado={() =>  setVisivel('tabela')}
+          cancelado={() =>  exibirTabela}
           />
         )}
       </Layout>
