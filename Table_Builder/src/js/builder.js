67b32770d77ab5394320aa63edcd6732c383206f
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-19 02:02:44 -0300

    Table Data Compression

--- Table_Builder/src/js/builder.js
+++ Table_Builder/src/js/builder.js
@@ -18,6 +18,28 @@ new Vue({
             fetch("/data/sales.json")
                 .then(resposne => resposne.json())
                 .then(data => this.sales = data)
+        },
+        calculateTotal(sales) {
+            if (!sales) return 0;
+            return sales.reduce((total, sale) => total + sale.value, 0);
+        }
+    },
+    computed: {
+        salesByProduct() {
+            if (!this.sales) return null;
+
+            const salesByProduct = this.sales.reduce((grouped, sale) => {
+                if (!grouped.hasOwnProperty(sale.name)) {
+                    grouped[sale.name] = {
+                        name: sale.name,
+                        value: sale.price * sale.qtde
+                    }
+                    return grouped;
+                }
+                grouped[sale.name].value += sale.price * sale.qtde;
+                return grouped;
+            }, {});
+            return Object.values(salesByProduct);
         }
     },
     mounted() {
